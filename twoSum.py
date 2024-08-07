b2622704d404e52f6a9f2c237553989905d67fee
tachorzy <tachorzyad@gmail.com>
2024-08-07 00:41:15 -0500

    solution: two sum

--- twoSum.py
+++ twoSum.py
@@ -1,3 +1,5 @@
+# two-sum solution using a dictionary/hashmap O(n) time complexity
+# explanation: cache each value that you iterate over in a hashmap, and then check if the difference between the target and the current value is in the hashmap. If it is, return the index of the difference and the current index.
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         prevMap = {}
